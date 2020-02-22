1bdb909b6fa37396d6cc86edd9fe22a310a6c303
Vi <weikang0827@gmail.com>
2020-02-22 16:01:49 +0800

    22. Arrays

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -27,3 +27,40 @@ var whatDoYouDo = function (job, firstName) {
 console.log(whatDoYouDo('teacher', 'John'));
 console.log(whatDoYouDo('designer', 'Jane'));
 console.log(whatDoYouDo('retired', 'Mark'));
+
+
+
+
+
+/**********************************
+ * 22. Arrays
+ */
+
+ // Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+
+console.log(names.length);
+
+// Mutate array data
+names[5] = 'Mary';
+console.log(names); // (6) ["John", "Mark", "Jane", empty × 2, "Mary"]
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'teacher', false];
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+
+john.pop();
+console.log(john);
+
+john.shift();
+console.log(john);
+
+
+console.log(john.indexOf(1990));    // 2
+console.log(john.indexOf(23));      // -1
+
+var isDesigner = john.indexOf('designer') === -1 ? 
+'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);

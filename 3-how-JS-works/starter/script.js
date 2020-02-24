77c33b7bea7f4a475beb3389283afb6f476b5908
Vi <weikang0827@gmail.com>
2020-02-24 15:25:25 +0800

    39. Hoisting in Practice

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,16 +1,36 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// functions
+calculateAge(1965);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+
+// calculateAge(1990);
+
+// retirement(1956);   // does not work
+
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
 
 
+// variables
 
+console.log(age);   // undefined
+var age = 23;
+// console.log(age);
 
-
-
-
-
-
+function foo() {
+    console.log(age);   // undefined
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
